0ded1da6d2e101fcfcd1640b17203af4ac4e31c0
Vaish.Abhishek <vaish.abhi07@gmail.com>
2019-04-09 05:55:40 +0530

    setup firebase and ngbootstrap

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+// Angular Import
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { environment } from '../environments/environment';
+
+// Third party imports
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+// Component Imports
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase,'myapp'),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB3F0lrmcEPLy0MOMa_4Y2Vi4TXYNPe2Lo",
+    authDomain: "myapp-e96b1.firebaseapp.com",
+    databaseURL: "https://myapp-e96b1.firebaseio.com",
+    projectId: "myapp-e96b1",
+    storageBucket: "",
+    messagingSenderId: "298598101046"
+  }
+};
